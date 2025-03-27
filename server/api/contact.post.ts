769ebe655c7a541e753df4a216cb237d6a3851dd
Faisal HUSSAIN <faisal.hussain@outlook.fr>
2025-03-28 00:15:07 +0100

    Improve error handling for Discord webhook integration in contact form

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -9,15 +9,22 @@ export default defineEventHandler(async (event) => {
   messageForDiscord += `> **Message:**\n> ${body.message}`
 
   // Send webhook to Discord
-  fetch(discordContactWebhookUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      content: messageForDiscord,
-    }),
-  }).catch(() => {}) // Ignore errors
+  if (discordContactWebhookUrl) {
+    fetch(discordContactWebhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        content: messageForDiscord,
+      }),
+    }).catch((err) => {
+      console.error('Error sending message to Discord webhook', err)
+    })
+  }
+  else {
+    console.error('No Discord webhook URL provided in runtime config.')
+  }
 
   return { message: 'Data received successfully' }
 })
